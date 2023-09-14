8a2f1dd2503ffd1101b96fa8cc399a91c781e32c
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-09-14 12:12:25 +0530

    Update butterflyPattern.cpp

--- butterflyPattern.cpp
+++ butterflyPattern.cpp
@@ -1,4 +1,9 @@
-for (i = 1;i<=5;i++)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k;
+   for (i = 1;i<=5;i++)
     {
         for(j = 1;j<=i;j++)
         {
@@ -15,3 +20,21 @@ for (i = 1;i<=5;i++)
         }
         cout<<endl;
     }
+    for (i = 5;i>=1;i--)
+    {
+        for(j = 1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        k = 2*(5-i);
+        for(j = 0;j<k;j++)
+        {
+            cout<<" ";
+        }
+        for(j = 1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
