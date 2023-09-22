8f87ff62c7f6300cd7cc9c3123e3dddc6906fe08
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-09-22 12:49:19 +0530

    Update PolynomialAdder.cpp

--- PolynomialAdder.cpp
+++ PolynomialAdder.cpp
@@ -1,1 +1,54 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Polynomial{
+    int coeff;
+    int power;
+    Polynomial *next;
+};
+class PolyList{
+    public:
+    void setPolynomial(string s,Polynomial *head)//assuming no spaces present
+    {
+        int i = 0;
+        int power,num;
+        Polynomial *walker = head;
+        Polynomial *temp;
+        while(s[i] != ' '){
+            if(s[i]=='-')
+            {
+                i++;
+                num = -((int)s[i] - 48);
+                //go three positions ahead as x^ format
+                i+=3;
+                power=(int)s[i] -48;
+            }
+            else if(s[i] == '+')
+            {
+                i++;
+                num = (int)s[i] - 48;
+                i += 3;
+                power=(int)s[i] -48; 
+            }
+            else{//if first char is number
+                num = (int)s[0] - 48;
+                i = 3;
+                power=(int)s[3]-48;
+            }
+            walker->coeff = num;
+            walker->power = power;
+            i++;
+            walker = walker -> next;
+            cout<<num<<" "<<power<<endl;
+        }
+    }
+};
+struct Polynomial *poly1;
+int main()
+{
+    PolyList p1;
+    poly1 = new Polynomial;
+    p1.setPolynomial("3x^4+2x^3",poly1);
+}
