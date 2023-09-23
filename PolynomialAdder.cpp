7feff683834a446788073f5459828a38d69cd77a
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-09-23 13:52:40 +0530

    Update PolynomialAdder.cpp

--- PolynomialAdder.cpp
+++ PolynomialAdder.cpp
@@ -1,22 +1,18 @@
-
 #include<iostream>
 #include<string>
 using namespace std;
-
-struct Polynomial{
-    int coeff;
+class Polynomial{
     int power;
+    int coeff;
     Polynomial *next;
-};
-class PolyList{
     public:
-    void setPolynomial(string s,Polynomial *head)//assuming no spaces present
+    void setPolynomial(string s,Polynomial *head)//assuming no spaces present creates a Polynomial from string
     {
         int i = 0;
         int power,num;
-        Polynomial *walker = head;
-        Polynomial *temp;
+        Polynomial *curr = head;
         while(s[i] != ' '){
+            Polynomial *temp = new Polynomial;
             if(s[i]=='-')
             {
                 i++;
@@ -34,21 +30,75 @@ class PolyList{
             }
             else{//if first char is number
                 num = (int)s[0] - 48;
-                i = 3;
+                i += 3;
                 power=(int)s[3]-48;
             }
-            walker->coeff = num;
-            walker->power = power;
-            i++;
-            walker = walker -> next;
-            cout<<num<<" "<<power<<endl;
+           i++;
+           curr->power = power;
+           curr->coeff = num;
+           curr->next = temp;
+           curr=temp;
+        }
+    }
+    void add(Polynomial *p3,Polynomial *p1,Polynomial *p2)
+    {
+        Polynomial *temp3 = p3;
+        while(p1!=NULL && p2!=NULL)
+        {
+            if(p1->power== p2->power)//if equal
+            {
+                temp3->power = p1->power;
+                temp3->coeff = p1->coeff + p2->coeff;
+                temp3->next = new Polynomial;
+                temp3 = temp3->next;
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+            else if(p1->power > p2->power)
+            {
+                temp3->power = p1->power;
+                temp3->coeff = p1->coeff;
+                temp3->next = new Polynomial;
+                temp3 = temp3->next;
+                p1 = p1->next;
+            }
+            else
+            {
+                temp3->power = p2->power;
+                temp3->coeff = p2->coeff;
+                temp3->next = new Polynomial;
+                temp3 = temp3->next;
+                p2 = p2->next;
+            }
+        }
+    }
+    void print(Polynomial *head)
+    {
+        if(head==NULL)
+        {
+            return;
+        }
+        else{
+            if(head->coeff==0){
+                return;
+            }
+            else{
+                cout<<" + "<<head->coeff<<"x^"<<head->power;
+            }
+            print(head->next);
+            return;
         }
     }
 };
-struct Polynomial *poly1;
 int main()
 {
-    PolyList p1;
-    poly1 = new Polynomial;
-    p1.setPolynomial("3x^4+2x^3",poly1);
+    Polynomial *p1= new Polynomial;
+    Polynomial *p2 = new Polynomial;
+    Polynomial *sum = new Polynomial;
+    p1->setPolynomial("4x^6+5x^4+3x^3+4x^2+3x^1 ",p1);
+    p2->setPolynomial("4x^5+4x^3-2x^1 ",p2);
+    sum->add(sum,p1,p2);
+    cout<<"Sum of Polynomials: 4x^6+5x^4+3x^3+4x^2+3x^1 and 4x^5+4x^3-2x^1"<<endl;
+    cout<<"Is ";
+    sum->print(sum);
 }
