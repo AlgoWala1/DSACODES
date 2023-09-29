af59619bf2904151744928d14b66916f37f4342a
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-09-29 11:20:35 +0530

    Update Insertionsort.cpp

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -17,41 +17,14 @@ int main()
      for(k = 1;k < SIZE;k++)
     {
       j = k - 1;
-      while(j>=0)//Traverse through list
+      while(j>=0 && toSort[j]>toSort[j+1])//Traverse through list(with all elements before assumed to be sorted)
       {
-          if(toSort[j]>toSort[k])//continue traversing
-          {
-              j--;
-              if(j == -1)//shift from the starting(start of the list is reached)
-              {
-                  swap = toSort[k];
-                  for(j = k;j>0;j--)
-                  {
-                      toSort[j] = toSort[j-1];
-                  }
-                  toSort[0] = swap;
-              pass++;
-              break;
-              }
-
-          }
-        else if(toSort[k]>toSort[j])//switch position with one after j(if no switch required code wouldnt enter loop)
-          { 
-            int b = j+1;
-            swap = toSort[k];
-            for(j = k;j>b;j--)
-            {
-                toSort[j] = toSort[j-1];
-            }
-            toSort[b] = swap;
-            if(j!=k){pass++;}
-            break;
-          }
-          else
-          {
-              break;
-          }
+          swap = toSort[j];
+          toSort[j] = toSort[j+1];
+          toSort[j+1] = swap;
+          j--;
       }
+      pass++;
     }
     cout<<"\nList: ";
     for(k = 0;k<SIZE;k++)
