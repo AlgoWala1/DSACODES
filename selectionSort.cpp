79b87a25666e23f2c4654e614ad3fdcc533cb98b
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2024-03-19 15:53:19 +0530

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+int main(){
+    //selection sort
+    int minIndex = 0;
+    int swap;
+    vector<int> ele = {1,2,3,-4,2,0,7,2,-2,1};
+    for(int i = 0;i < ele.size();i++){
+        minIndex = i;
+        for(int j = i;j<ele.size();j++){
+            if(ele[j]<ele[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap = ele[minIndex];
+        ele[minIndex] = ele[i];
+        ele[i] = swap;
+    }
+    cout<<"Elements after sorting"<<endl;
+    for(auto i:ele){
+        cout<<i<<" ";
+    }
+}
